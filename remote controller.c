ca6f0b57dee863380f7eda0f3e8dbe03d1f5666a
2saewoo <39489835+leesaewoo@users.noreply.github.com>
2019-02-27 23:56:30 +0900

    remote controller renew

--- remote controller.c
+++ remote controller.c
@@ -9,7 +9,7 @@ int Compare_plus_minus_100(); //가장 적게 누른 횟수 구하는 함수
 
 int main(void)
 {
-	int i, j, k, l, m, temp, check = 0;
+	int i, j, k, l, m, temp, counter = 0;
 	int channel_int; //원하는 채널
 	int channel_count; //원하는 채널의 자릿수
 	int button_count; // 고장난 버튼의 갯수 
@@ -20,8 +20,9 @@ int main(void)
  	int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int *pa = array;
 	int array_max; // 고장난 버튼을 제외하고 가장 큰 버튼 값
-	int array_min; // 고장난 버튼을 제외하고 가장 작은 버튼 값
-		
+	int array_min1, array_min2 = -1; // 고장난 버튼을 제외하고 가장 작은 버튼 값
+	int button_push; // 원하는 채널을 보기 위해 가장 적게 버튼을 누른 횟수 
+
 	while(1)
 	{
 		scanf("%d", &channel_int); // 원하는 채널 입력 
@@ -66,11 +67,15 @@ int main(void)
 			break;
 		}
 	}
-	for(i=0;i<10;i++) // array_min 구하는 함수 
+	for(i=0;i<10;i++) // array_min1, array_min2 구하는 함수 
 	{
-		if(*(pa+i) != -1)
+		if(*(pa+i) != -1 && array_min1 == -1)
 		{
-			array_min = *(pa+i);
+			array_min1 = *(pa+i);
+		}
+		else if(*(pa+i) != -1 && array_min1 != -1)
+		{
+			array_min2 = *(pa+i);
 			break;
 		}
 	}
@@ -84,233 +89,94 @@ int main(void)
 		temp -= pow(10,channel_count -1 -i)*(*(pcc+i));
 	}
 
-	for(i=0;i<channel_count;i++) // cmp_plus 구하기 
-	{
-		if(channel_count == 1)
-		{
-			if(*(pa + *pcc) == -1 && *pcc != 0)
-			{
-				for(i=1;i<*pcc;i++)
-				{	
-					if(*(pa + *pcc - i) != -1)
-					{
-						cmp_plus = *(pa + *pcc - i);
-						break;
-					} 
-				}
-			}
-			else if(*(pa + *pcc) == *pcc)
-			{
-				cmp_plus = 0;
-				break;
-			}
-			else
-			{
-				cmp_plus = 9999999;
-				break;
-			}
-		}
-				
-		// 0 ~ channel_count -2 까지 판별식
 
-		if(*(pa + *(pcc+i)) != -1 && i < channel_count -1 && channel_count != 1)
+
+	// 공통사항 (숫자 버튼을 누르는 행동을 취할 때 counter++ (+-는 제외)) 
+
+	if(button_count == 0) // 고장난 버튼이 없을 때
+	{
+		printf("%d", channel_count); // button_push = channel_count 
+	}
+	else if(button_count == 10) // 버튼이 모두 고장났을 때
+	{
+		printf("%d", Compare_100(channel_int)); // Compare_100(channel_int)
+	}
+	else if(button_count == 9 && *pa == 0) // 버튼이 0 빼고 모두 고장났을 때
+	{
+		counter = 1; //버튼은 무조건 한번만 누르는거 (0) 
+		button_push = counter + channel_int;
+		if(button_push >= Compare_100(channel_int))
 		{
-			cmp_plus += pow(10,channel_count -i - 1) * (*(pa + *(pcc+i)));
+			printf("%d", Compare_100(channel_int));
 		}
-		else if(*(pa + *(pcc+i)) == -1 && i < channel_count -1 && channel_count != 1)
+		else
 		{
-			for(j=1;j<=*(pcc+i);j++)
-			{
-				if(*(pa + *(pcc+i) - j) == 0 && i == 0) // 한 자릿수 낮은 가장 큰 수에서 ++ 경우
-				{
-					cmp_plus = 0;
-						
-					for(l=1;l<channel_count;l++)
-					{
-						cmp_plus += pow(10,channel_count - l - 1) * array_max;
-					}
-					check = 77;				
-					i = channel_count;
-					break;
-				}
-				else if(*(pa + *(pcc+i) - j) != -1 && *(pcc+i) - j >= 0)
-				{
-					cmp_plus += pow(10,channel_count -i -1) * *(pa + *(pcc+i) - j);
-					
-					for(k=1;channel_count - i - 1 - k >= 0; k++)
-					{
-						cmp_plus += pow(10,channel_count -i -1 -k) * array_max;
-					}
-					i = channel_count;
-					j = *(pcc+i) + 1;
-					break;
-				}
-			}
+			printf("%d", button_push);
 		}
 		
-		if(i == channel_count -1)
+	}
+	else // 그 외의 경우
+	{
+		counter = 0;
+		
+		for(i=0 ; i<channel_count ; i++) //cmp_plus
 		{
-			if(*(pa + *(pcc+i)) != -1)
+			if(i == 0)
 			{
-			cmp_plus += (*(pa + *(pcc+i)));
-			}
-			else if((*(pa + *(pcc+i)) == -1))
-			{
-				for(j=1;j<=*(pcc+i);j++)
+				if(*(pa + *pcc) != -1) // 첫 번째 자리는 같거나 
 				{
-					if(*(pa + *(pcc+i) - j) >= 0 && *(pa + *(pcc+i) - j) < 10 && i-j >= 0)
-					{
-						cmp_plus += (*(pa + *(pcc+i) - j));
-						break;
-					}
-					else if(j == *(pcc+i))
-					{
-						cmp_plus = 9999999;
-						break;
-					}	
+					cmp_plus += pow(10, channel_count -1) * *(pa + *pcc);
+					counter++;
 				}
-			}
-		}		
-	}
-	if(cmp_plus == channel_int && check != 77) // cmp_plus_count 구하기
-	{
-		cmp_plus_count = channel_count;
-	}
-	else if(check == 77)
-	{
-		cmp_plus_count = abs(cmp_plus - channel_int) + channel_count - 1;
-	}
-	else if(button_count == 9 && *pa == 0)
-	{
-		cmp_plus_count = channel_int + 1;
-	}
-	else if(check != 77)
-	{
-		cmp_plus_count = abs(cmp_plus - channel_int) + channel_count;
-	}	
-	
-	
-	for(i=0;i<channel_count;i++) // cmp_minus 구하기 
-	{
-		if(channel_count == 1)
-		{
-			if(*(pa + *pcc) == -1)
-			{
-				for(i=1;i<(10 - *pcc);i++ && *pcc != 9)
-				{	
-					if(*(pa + *pcc + i) != -1)
-					{
-						cmp_minus = *(pa + *pcc + i);
-						break;
-					} 
-				}
-			}
-			else if(*(pa + *pcc) == *pcc)
-			{
-				cmp_minus = 0;
-				break;
-			}
-			else
-			{
-				cmp_minus = 9999999;
-				break;
-			}
-			break;
-		}
-
-		if(i == channel_count -1)
-		{
-			if(*(pa + *(pcc+i)) != -1)
-			{
-			cmp_minus += *(pa + *(pcc+i));
-			}
-			else
-			{
-				for(j=1;j<(10 - *(pcc+i));j++)
+				else if(*(pa + *pcc) == -1)
 				{
-					if(*(pa + *(pcc+i) + j) != -1)
+					for(j = 1 ; j <= *pcc ; j++)
 					{
-						cmp_minus += (*(pa + *(pcc+i) + j));
-						break;
-					}
-					else // 한 자리 많은 최소값에서 -- 하는 경우 
-					{
-						cmp_minus = 0;
-						
-						for(l=0;l<channel_count;l++)
+						if(*(pa + *pcc - j) != -1 && j != *pcc) // 작은 수 중 가장 큰 수 
 						{
-							if(l ==0 && array_min != 0)
+							cmp_plus += pow(10, channel_count -1) * *(pa + *pcc - j);
+							counter++;
+							if(channel_count >1)
 							{
-								cmp_minus += pow(10,channel_count - l) * array_min;
-								check = 7;
-							}
-							else if(l ==0 && array_min == 0)
-							{
-								for(m=1;m<10;m++)
+								for(k = channel_count -2 ; k >=0 ; k--)
 								{
-									if(*(pa + m) != -1)
-									{
-										cmp_minus += pow(10,channel_count - l) * *(pa + m);
-										check = 7;
-									}
-									else if(m == 9 && *(pa + m) == -1)
-									{
-										cmp_minus = 9999999;
-										l = channel_count;
-										break;
-									}
-								} 
+									cmp_plus += pow(10, k) * array_max;
+									counter++;
+								}								
 							}
+							cmp_plus_count = abs(cmp_plus - channel_int) + counter;
+							i = channel_count;
+							break;
+						}
+						else if(j == *pcc) // 첫 번째 자리에 가능한 수가 0보다 작거나 같을 경우 
+						{
+							for(k = channel_count - 2 ; k >= 0  ; k--)
+							{
+								cmp_plus += pow(10, k) * array_max;
+								counter++;
+							}
+							cmp_plus_count = abs(cmp_plus - channel_int) + counter;
+							i = channel_count;
 						}
 					}
 				}
 			}
-		}
-		
-		// 0 ~ channel_count -2 까지 판별식
-
-		if(*(pa + *(pcc+i)) != -1 && i < channel_count -1 )
-		{
-			cmp_minus += pow(10,channel_count -i - 1) * (*(pa + *(pcc+i)));
-		}
-		else if(*(pa + *(pcc+i)) == -1 && i < channel_count -1)
-		{
-			for(j=1;j<(10 - *(pcc + i));j++)
+			else // 나머지 자리는 같거나 작은 수 중 가장 큰 수 
 			{
-				if(*(pa + *(pcc+i) + j) == -1 && i == 0 && *(pcc+i) + j == 9)
-				{
-					check = 8;
-				}
-				else if(*(pa + *(pcc+i) + j) != -1 && *(pcc+i) + j <= 9 )
-				{
-					cmp_minus += pow(10,channel_count -i -1) * *(pa + *(pcc+i) + j);
-					
-					for(k=1;channel_count - i - 1 - k >= 0; k++)
-					{
-						cmp_minus += pow(10,channel_count -i -1 -k) * array_min;
-					}
-					i = channel_count;
-					break;
-				}
+				
 			}
 		}
 	}
-	if(cmp_minus == channel_int) // cmp_minus_count 구하기
-	{
-		cmp_minus_count = channel_count;
-	}
-	else if(check == 7)
-	{
-		cmp_minus_count = abs(cmp_minus - channel_int) + channel_count + 1;	
-	}
-	else if(check == 8)
-	{
-		cmp_minus_count = 999999;
-	}
-	else
-	{
-		cmp_minus_count = abs(cmp_minus - channel_int) + channel_count;
-	}
+ 
+		//cmp_minus
+			//첫째 자리는  같거나, 큰 수 중 가장 작은 수
+			//나머지 자리는 누를수 있는 수 중 가장 작은 수로 
+			//첫째 자리에 가능한 수가 9보다 큰 경우 
+				//원하는 채널보다 한 자릿수 많은 채널에서 -- 
+				//첫째 자리는 0이 아닌 가장 작은 수
+				//나머지 자리는 누를 수 있는 수 중 가장 작은 수 (0도 가능) 
+		//Compare_100(channel_int) 
+
 
 
 	for(i=0;i<channel_count;i++) // 원하는 채널 char 배열 확인 
@@ -340,8 +206,7 @@ int main(void)
 	
 	printf("Compare_100 = %d\n",Compare_100(channel_int));
 	
-	printf("array_max = %d\narray_min = %d\n",array_max,array_min);
-	
+	printf("array_max = %d\narray_min1 = %d\narray_min2 = %d\n",array_max,array_min1,array_min2);
 	
 	printf("채널을 맞출 때 버튼을 가장 적게 누르는 횟수 = %d\n",Compare_plus_minus_100(button_count,cmp_plus_count,cmp_minus_count,Compare_100(channel_int),channel_count));
 
@@ -416,3 +281,13 @@ int Compare_plus_minus_100(int b,int p,int m,int c,int cc)
 		return c;
 	}
 }
+
+
+
+
+
+
+
+
+
+
