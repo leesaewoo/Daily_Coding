a0b953fe39089c32c35f8489d3e27bbcc3f52dde
2Saewoo <39489835+leesaewoo@users.noreply.github.com>
2023-04-21 14:20:56 +0900

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,72 @@
+package com.codestates.seb.calculator;
+import java.util.Scanner;
+public class Calculator {
+  public static void main(String[] args) {
+        /*
+            요구 사항에 따라 간단한 계산기를 만들어주세요.
+            1. 사용자의 입력으로 첫 번째 숫자, 연산자, 두 번째 숫자를 받아야 합니다.
+            2. 연산자의 종류는 +, -, *, / 입니다.
+            3. 소수점 연산을 수행할 수 있어야 합니다.
+            4. 연산 결과를 콘솔에 출력합니다.
+        */
+    System.out.println("===Java Calculator===");
+
+    Scanner scanner = new Scanner(System.in);
+
+    while(true) {
+      System.out.println("계산식을 입력해 주세요. (종료: \"end\" 입력)");
+      System.out.println("Ex) 3 + 4");
+
+      try {
+        String inputString = scanner.nextLine();
+        if(inputString.equalsIgnoreCase("end")) {
+          System.out.println("종료되었습니다.");
+          break;
+        }
+
+        double firstNum = 0;
+        double secondNum = 0;
+        char operator = ' ';
+
+        //입력값(inputString)에서 첫 번째 숫자(firstNum), 연산자(operator), 두 번째 숫자(secondNum)를 추출
+        //입력값 첫 자리에 음수를 나타내는 '-'가 들어올 수도 있기 때문에 두 번째 자리부터 탐색
+        for(int i = 1 ; i < inputString.length() ; i++) {
+          if(inputString.charAt(i) == '+' || inputString.charAt(i) == '-' || inputString.charAt(i) == '*'
+                  || inputString.charAt(i) == '/') {
+            firstNum = Double.parseDouble(inputString.substring(0, i).trim());
+            secondNum = Double.parseDouble(inputString.substring(i + 1, inputString.length()).trim());
+            operator = inputString.charAt(i);
+            break;
+          }
+
+          if(i == inputString.length() - 1) {
+            throw new Exception();
+          }
+        }
+
+        switch(operator) {
+          case '+':
+            System.out.printf("계산된 값 : %.4f\n\n", firstNum + secondNum);
+            break;
+
+          case '-':
+            System.out.printf("계산된 값 : %.4f\n\n", firstNum - secondNum);
+            break;
+
+          case '*':
+            System.out.printf("계산된 값 : %.4f\n\n", firstNum * secondNum);
+            break;
+
+          case '/':
+            System.out.printf("계산된 값 : %.4f\n\n", firstNum / secondNum);
+            break;
+
+          default:
+            throw new Exception();
+        }
+      } catch (Exception e) {
+        System.out.println("잘못된 입력입니다. 다시 입력해 주세요");
+      }
+    }
+  }
+}
