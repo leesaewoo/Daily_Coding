c72cb57cb1ab01684430ffebf4873cfb5b47a1d0
2Saewoo <39489835+leesaewoo@users.noreply.github.com>
2023-05-19 18:09:00 +0900

    Add files via upload

--- BoringBlackjack.java
+++ BoringBlackjack.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 //평범한 블랙잭 게임에서 수시로 패배하자 흥미가 떨어진 김코딩은
 // 박타짜에게 게임룰을 변형하여 새로운 카드 놀이를 해 볼 것을 제안합니다.
 //새로운 룰은 다음과 같습니다.
@@ -18,26 +19,40 @@
 //중복된 숫자는 들어있지 않습니다.
 //배열의 요소는 1이상 1,000이하의 자연수입니다.
 public class BoringBlackjack {
+    ArrayList<Integer> result = new ArrayList<>();
     public int boringBlackjack(int[] cards) {
         //여러 장의 카드 중 세 장씩 조합해 소수가 되는 경우의 수를 리턴
         //중복된 숫자는 없음
         //소수인지 판별하는 메서드 필요함
         //주어진 카드 중 세 장(순서 상관 X, 중복 X) 골라서 합한 수를 소수판별하여 소수면 count++
         //고르는 카드의 수가 정해져 있으므로, 3중 반복문 사용
-        int sum;
+        recursion(cards, 0, 3, 0);
         int count = 0;
-        for(int i = 0 ; i < cards.length - 2 ; i++) {
-            for(int j = i + 1 ; j < cards.length - 1 ; j++) {
-                for(int k = j + 1 ; k < cards.length ; k++) {
-                    sum = cards[i] + cards[j] + cards[k];
-                    if(isPrimeNumber(sum)) {
-                        count++;
-                    }
-                }
+        for(int i = 0 ; i < result.size() ; i++) {
+            if(isPrimeNumber(result.get(i))) {
+                count++;
             }
         }
         return count;
     }
+
+    public void recursion(int[] cards, int index, int choiceNum, int sum) {
+        if(choiceNum == 0) {
+            result.add(sum);
+            return;
+        }
+
+        if(index > cards.length - choiceNum) {
+            return;
+        }
+
+        //뽑거나
+        int newSum = sum + cards[index];
+        recursion(cards, index + 1, choiceNum - 1, newSum);
+        //안뽑거나
+        recursion(cards, index + 1, choiceNum, sum);
+    }
+
     //소수인지 판별하는 메서드
     public boolean isPrimeNumber(int number) {
         for(int i = 2 ; i < number / 2 ; i++) {
